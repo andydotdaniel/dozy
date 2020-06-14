8da511781029b1cda51940572f5a625649feab88
Andrew Daniel <andydotdaniel@gmail.com>
2020-06-14 13:18:55 +0700

    Initial Commit

---
+++ Dozy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dozy
+//
+//  Created by Andrew Daniel on 6/14/20.
+//  Copyright © 2020 Andrew Daniel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
