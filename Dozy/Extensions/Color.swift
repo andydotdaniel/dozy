c844a100d6502751b5e4a137afe69e08ed9ff11a
Andrew Daniel <andydotdaniel@gmail.com>
2020-06-14 21:15:56 +0700

    Add primaryBlue and borderGray color extensions

---
+++ Dozy/Extensions/Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  Dozy
+//
+//  Created by Andrew Daniel on 6/14/20.
+//  Copyright © 2020 Andrew Daniel. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    static let primaryBlue = Color(red: 47/255, green: 155/255, blue: 255/255)
+    static let borderGray = Color(red: 225/255, green: 225/255, blue: 225/255)
+    
+}
