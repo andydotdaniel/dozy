04a8ff89a6fa73fc570710b93a4233ec1aae8070
Andrew Daniel <andydotdaniel@gmail.com>
2020-06-16 00:33:57 +0700

    Add state parameter to Slack auth request

--- Dozy/Scenes/Login/LoginPresenter.swift
+++ Dozy/Scenes/Login/LoginPresenter.swift
@@ -15,30 +15,37 @@ protocol LoginViewPresenter: class {
 
 final class LoginPresenter: LoginViewPresenter {
     
-    private let authUrl: URL = {
-        var urlComponents = URLComponents(string: "https://slack.com/oauth/v2/authorize")!
-        urlComponents.queryItems = [
-            URLQueryItem(name: "client_id", value: Current.configuration.clientId),
-            URLQueryItem(name: "user_scope", value: "chat:write,channels:read,groups:read"),
-            URLQueryItem(name: "redirect_url", value: "dozyapp://slack/authorize/success"),
-        ]
-        return urlComponents.url!
-    }()
+    private let authUrl: URL
+    private let authRequestIdentifier: String
     
     private var authenticationPresentationContext: ASWebAuthenticationPresentationContextProviding?
     private let networkService: NetworkRequesting
     
     init(networkService: NetworkRequesting) {
         self.networkService = networkService
+                
+        self.authRequestIdentifier = UUID().uuidString
+        var urlComponents = URLComponents(string: "https://slack.com/oauth/v2/authorize")!
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Current.configuration.clientId),
+            URLQueryItem(name: "user_scope", value: "chat:write,channels:read,groups:read"),
+            URLQueryItem(name: "redirect_url", value: "dozyapp://slack/authorize/success"),
+            URLQueryItem(name: "state", value: self.authRequestIdentifier)
+        ]
+        self.authUrl = urlComponents.url!
     }
     
     func didTapLoginButton() {
         let session = ASWebAuthenticationSession(url: authUrl, callbackURLScheme: "dozyapp") { [weak self] callbackURL, error in
-            guard error == nil, let callbackURL = callbackURL else { return }
+            guard error == nil, let callbackURL = callbackURL, let self = self else { return }
             
             let queryItems = URLComponents(string: callbackURL.absoluteString)?.queryItems
+            
+            guard let state = queryItems?.first(where: { $0.name == "state" })?.value,
+                state == self.authRequestIdentifier else { return }
+            
             queryItems?.first { $0.name == "code" }.map { codeQueryItem in
-                guard let requestCode = codeQueryItem.value, let self = self else { return }
+                guard let requestCode = codeQueryItem.value else { return }
                 self.requestAccessToken(with: requestCode)
             }
         }
