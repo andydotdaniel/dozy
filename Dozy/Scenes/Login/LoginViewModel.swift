b030c0e0ff457b718375e5d436fd828601484702
Andrew Daniel <andydotdaniel@gmail.com>
2020-06-17 00:35:04 +0700

    Add Login view model

---
+++ Dozy/Scenes/Login/LoginViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewModel.swift
+//  Dozy
+//
+//  Created by Andrew Daniel on 6/16/20.
+//  Copyright © 2020 Andrew Daniel. All rights reserved.
+//
+
+import Combine
+import SwiftUI
+
+class LoginViewModel: ObservableObject {
+    
+    @Published var isFetchingAccessToken: Bool = false
+    
+}
