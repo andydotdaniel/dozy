a874529d232f9e55f3bd10ec0c957203fc5ddb8c
Andrew Daniel <andydotdaniel@gmail.com>
2020-07-20 23:21:54 +0700

    Add Message model

---
+++ Dozy/Common/Models/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  Dozy
+//
+//  Created by Andrew Daniel on 7/19/20.
+//  Copyright © 2020 Andrew Daniel. All rights reserved.
+//
+
+import UIKit
+
+struct Message {
+    let image: UIImage?
+    let bodyText: String?
+    let channel: Channel
+    let awakeConfirmationTime: Date
+}
