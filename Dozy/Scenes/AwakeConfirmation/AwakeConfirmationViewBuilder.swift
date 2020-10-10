e85b31e2fa2cb16e42850371b0f5a190a91f9187
Andrew Daniel <andydotdaniel@gmail.com>
2020-10-10 14:04:22 +0800

    Add awake confirmation view builder

---
+++ Dozy/Scenes/AwakeConfirmation/AwakeConfirmationViewBuilder.swift
@@ -0,0 +1,27 @@
+//
+//  AwakeConfirmationViewBuilder.swift
+//  Dozy
+//
+//  Created by Andrew Daniel on 10/10/20.
+//  Copyright © 2020 Andrew Daniel. All rights reserved.
+//
+
+import Foundation
+
+struct AwakeConfirmationViewBuilder: ViewBuilder {
+    
+    private let schedule: Schedule
+    
+    init(schedule: Schedule) {
+        self.schedule = schedule
+    }
+    
+    func build() -> AwakeConfirmationView {
+        let secondsLeft = schedule.awakeConfirmationTime.timeIntervalSinceNow
+        let viewModel = AwakeConfirmationViewModel(countdownActive: true, secondsLeft: Int(secondsLeft))
+        let presenter = AwakeConfirmationPresenter(viewModel: viewModel, networkService: NetworkService(), keychain: Keychain(), userDefaults: UserDefaults.standard, savedSchedule: schedule)
+        
+        return AwakeConfirmationView(viewModel: viewModel, presenter: presenter)
+    }
+    
+}
